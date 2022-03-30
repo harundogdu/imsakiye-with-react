139c44ed35b363f051f32ea2ddd1ed7fd6944c76
Harun Doğdu <harundogdu06@gmail.com>
2022-03-31 00:11:35 +0300

    tailwind css operation done

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "primary": "#3A3845",
+        "secondary" : "#313552",
+        "tertiary" : "#EFEFEF",
+      }
+    },
+  },
+  plugins: [],
+}
